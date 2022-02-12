ab47f85fea58362ef993b96a9c54f4e99d233e79
Даниил Королев <kdvikt@gmail.com>
2022-02-12 23:09:11 +0400

    Init

---
+++ Lab-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
